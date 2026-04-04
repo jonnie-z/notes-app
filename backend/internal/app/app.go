f6cac5454575ff5fbad958475bf5998d18ae99d2
David J. Sellinger <jonnie@jonniez.com>
2026-04-04 12:51:38 -0400

    starting sql

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -1,12 +1,17 @@
 package app
 
-import "github.com/jonnie-z/notes-app/internal/store"
+import (
+	"log"
+
+	"github.com/jonnie-z/notes-app/internal/store"
+)
 
 type App struct {
 	Store store.NoteRepository
 }
 
 func NewApp(storeType store.StoreType) *App {
+	dsn := "./notes.db"
 	var appStore store.NoteRepository
 
 	switch storeType {
@@ -14,6 +19,13 @@ func NewApp(storeType store.StoreType) *App {
 		appStore = store.NewNoteStore()
 	case store.StoreInMemory:
 		appStore = store.NewInMemoryStore()
+	case store.StoreSQL:
+		s, err := store.NewSQLiteStore(dsn)
+		if err != nil {
+			log.Fatalf("Err creating sql store! %s", err.Error())
+		}
+
+		appStore = s
 	}
 
 	return &App{
--- backend/internal/store/db_sql.go
+++ backend/internal/store/db_sql.go
@@ -1,8 +1,38 @@
 package store
 
-import "database/sql"
+import (
+	"database/sql"
+
+	_ "modernc.org/sqlite"
+)
 
 type SQLiteStore struct {
-	db *sql.DB
+	DB *sql.DB
 }
 
+func NewSQLiteStore(dsn string) (*SQLiteStore, error) {
+	db, err := sql.Open("sqlite", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	store := &SQLiteStore{
+		DB: db,
+	}
+
+	store.DB.Exec(`CREATE TABLE IF NOT EXISTS notes (
+    	id INTEGER PRIMARY KEY AUTOINCREMENT,
+    	body TEXT NOT NULL
+	);`)
+
+	return store, nil
+}
+
+func (s *SQLiteStore) GetAll() ([]Note, error) { return []Note{}, nil }
+func (s *SQLiteStore) Create(text string) (Note, error) { return Note{}, nil }
+func (s *SQLiteStore) Update(id int, text string) (Note, error) { return Note{}, nil }
+func (s *SQLiteStore) Delete(id int) error { return nil }
